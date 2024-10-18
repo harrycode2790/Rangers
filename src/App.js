755dbe20e00187f9a3f631274847f61bf1642f53
harry <hillarybond10@gmail.com>
2024-10-18 14:17:12 +0100

    make some changes and installed twailind css to the project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-
-import './App.css';
+import{BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Layout from './components/layout';
+import HomePage from "./components/HomePage";
 
 function App() {
   return (
-   <div>hello world </div>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path='/' element={ <HomePage/> } />
+        </Routes>
+      </Layout>
+    </Router>
+   
   );
 }
 
